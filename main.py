9ef0ba1e030601f77cbbdd5c28e48eaa2e2cce0e
Murasko <45008282+Murasko@users.noreply.github.com>
2022-09-21 18:32:44 +0200

    v0.1.0 - Initial Code push

---
+++ main.py
@@ -0,0 +1,99 @@
+#  Mako - Multipurpose python bot
+#  Copyright (c) 2022. Marco Murawski
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#  Contact:
+#  info@murasko.de
+#
+
+import asyncio
+import json
+import logging
+import os
+import platform
+import sys
+
+import aiosqlite
+import discord
+
+if not os.path.isfile("config.json"):
+    sys.exit("Couldn't find 'config.json'! Please make sure you've added it.")
+else:
+    with open("config.json") as config_file:
+        config = json.load(config_file)
+
+logger = logging.getLogger("discord")
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+
+handler.setFormatter(
+    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+)
+logger.addHandler(handler)
+
+intents = discord.Intents.all()
+
+bot = discord.Bot(intents=intents)
+
+bot.config = config
+
+
+async def init_database() -> None:
+    async with aiosqlite.connect("mako/db/database.sqlite") as database:
+        with open("mako/db/schema.sql") as database_schema:
+            await database.executescript(database_schema.read())
+        await database.commit()
+
+
+@bot.event
+async def on_ready() -> None:
+    print()
+    print(f"Logged in as {bot.user}")
+    print(f"py-cord API version: {discord.__version__}")
+    print(f"Python version: {platform.python_version()}")
+    print(f"Running on: {platform.system()} {platform.release()} ({os.name})")
+    print()
+    await change_discord_status()
+    print()
+
+
+async def change_discord_status() -> None:
+    await bot.change_presence(
+        activity=discord.Activity(
+            type=discord.ActivityType.watching,
+            name="über euch alle",
+            state=discord.Status.online,
+        )
+    )
+    print("Status set!")
+
+
+def load_cogs() -> None:
+    for file in os.listdir("mako/cogs"):
+        if file.endswith(".py"):
+            extension = file[:-3]
+            try:
+                bot.load_extension(f"mako.cogs.{extension}")
+                print(f"Loaded Extension {extension}")
+            except Exception as e:
+                exception = f"{type(e).__name__}: {e}"
+                print(f"Failed to load extension {extension}\n{exception}")
+
+
+load_cogs()
+
+if __name__ == "__main__":
+    asyncio.run(init_database())
+    bot.run(config["discord_token"])
