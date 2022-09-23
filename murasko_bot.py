debd91af7237c172ca64b40fdcdd4029fa3c3e8a
Murasko <github@murasko.de>
2022-09-23 14:56:50 +0200

    small change to script

--- murasko_bot.py
+++ murasko_bot.py
@@ -1,4 +1,3 @@
-from distutils.cmd import Command
 import discord
 from discord.ext import commands
 import os
@@ -7,25 +6,22 @@ from dotenv import load_dotenv
 load_dotenv()
 token = os.getenv('TOKEN')
 
+class MuraskoBot(discord.Client):
+    async def on_ready(self):
+        print(f'Bot {self.user} logged in!')
+    
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+    
+        if message.content.lower() == "hi":
+            await message.channel.send('Sup!')
+
+        if "käsekuchen" in message.content.lower():
+            await message.channel.send('Beste Kuchen!')
+
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='$')
-
-@client.event
-async def on_ready():
-    print(f'Bot {client.user} logged in!')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.lower() == "hi":
-        await message.channel.send('Sup!')
-
-    if "käsekuchen" in message.content.lower():
-        await message.channel.send('Beste Kuchen!')
-
-client.run(token)
+MuraskoBot = MuraskoBot(intents=intents)
+MuraskoBot.run(token)
