1fd55463085c87cf5c1c42afbede84b0e8b890b3
Murasko <github@murasko.de>
2022-10-12 19:06:31 +0200

    moved twitch api stuff to .env

--- MuraskoBot/twitch.py
+++ MuraskoBot/twitch.py
@@ -1,21 +1,28 @@
+import os
 import requests
 import json
 from datetime import datetime
+from dotenv import load_dotenv
+from pathlib import Path
 
 with open("config.json") as config_file:
     config = json.load(config_file)
 
+env_path = Path('.', ".env")
+load_dotenv(dotenv_path=env_path)
+
 
 def get_app_access_token():
     params = {
-        "client_id": config["client_id"],
-        "client_secret": config["client_secret"],
+        "client_id": os.getenv("CLIENT_ID"),
+        "client_secret": os.getenv("CLIENT_SECRET"),
         "grant_type": "client_credentials"
     }
 
     response = requests.post(
         "https://id.twitch.tv/oauth2/token", params=params)
     access_token = response.json()["access_token"]
+
     return access_token
 
 
@@ -25,7 +32,7 @@ def get_users(login_names):
     }
 
     headers = {
-        "Authorization": "Bearer {}".format(config["access_token"]),
+        "Authorization": f"Bearer {os.getenv('ACCESS_TOKEN')}",
         "Client-Id": config["client_id"]
     }
 
