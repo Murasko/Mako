bc5dbdb70c62938122367b0743cdb404cdbb7fe4
Murasko <git@murasko.de>
2024-03-29 20:30:53 +0100

    edit copyright

--- src/mako/db/__init__.py
+++ src/mako/db/__init__.py
@@ -1,5 +1,5 @@
 #  Mako - Python Discord Bot
-#  Copyright (c) 2023. Marco Murawski
+#  Copyright (c) 2024. Marco Murawski
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
